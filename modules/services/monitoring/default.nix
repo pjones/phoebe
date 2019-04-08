bd9408849d76c5133475ffb03389f942106b0968
Peter Jones <pjones@devalot.com>
2019-04-08 09:45:54 -0700

    Remove custom netdata plugin
    It didn't really work anyways.

--- modules/services/monitoring/default.nix
+++ modules/services/monitoring/default.nix
@@ -7,27 +7,12 @@ with lib;
 let
   cfg = config.phoebe.services.monitoring;
 
-  plugins = import ./plugins.nix { inherit (pkgs) stdenvNoCC netdata; };
-
   alarmNotifyConf = pkgs.writeText "health_alarm_notify.conf"
     (optionalString cfg.pushover.enable ''
       SEND_PUSHOVER=YES
       PUSHOVER_APP_TOKEN="${cfg.pushover.apiKey}"
       DEFAULT_RECIPIENT_PUSHOVER="${concatStringsSep "," cfg.pushover.userKeys}"
     '');
-
-  alarmConf = pkgs.writeText "alarms.conf" ''
-    # Send alarms for systemd services.
-      alarm: failed_service
-         on: Services.status
-         os: linux
-      hosts: *
-     lookup: min -1m unaligned
-      every: 1m
-       crit: $this < -3 && $this >= -4
-       info: service is failed
-         to: sysadmin
-  '';
 in
 {
   #### Interface
@@ -57,26 +42,16 @@ in
     # Use netdata to collect metrics:
     services.netdata = {
       enable = true;
-      extraPluginPaths = [ "${plugins}/plugins.d" ];
 
       config.global = {
         "debug log"  = "syslog";
         "access log" = "syslog";
         "error log"  = "syslog";
       };
-
-      config.plugins = {
-        "phoebe.charts.d.plugin" = "yes";
-      };
     };
 
     environment.etc."netdata/health_alarm_notify.conf" = {
       source = "${alarmNotifyConf}";
-      mode   = "0444";
-    };
-
-    environment.etc."netdata/health.d/alarm.conf" = {
-      source = "${alarmConf}";
       mode   = "0444";
     };
   };
